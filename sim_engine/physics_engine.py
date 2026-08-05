102f684fed9b3c7633926d0f18115da8ac09da78
Thomas Giguere Sturrock <elotryk@gmail.com>
2026-08-05 17:29:34 -0400

    feat(physics): implement kinematics calculations (Closes #1)

---
+++ sim_engine/physics_engine.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+
+@dataclass
+class Vector3D:
+    x: float = 0.0
+    y: float = 0.0
+    z: float = 0.0
+
+class PhysicsEngine:
+    def __init__(self, dt: float = 0.02, mass: float = 1.0, g: float = 9.81):
+        self.dt = dt
+        self.mass = mass
+        self.g = g
+
+        # State vectors
+        self.position = Vector3D()
+        self.velocity = Vector3D()
+        self.acceleration = Vector3D()
+
+    def step(self, external_forces: Vector3D = None) -> dict:
+        """Executes one simulation step (dt seconds)."""
+        if external_forces is None:
+            external_forces = Vector3D()
+
+        # 1 Calculate Acceleration ( F = ma -> a = F/m)
+        self.acceleration.x = external_forces.x / self.mass
+        self.acceleration.y = external_forces.y / self.mass
+        self.acceleration.z = (external_forces.z /self.mass) - self.g
+
+        # 2 Calculate Velocity ( v = v + a * dt )
+        self.velocity.x += self.acceleration.x * self.dt
+        self.velocity.y += self.acceleration.y * self.dt
+        self.velocity.z += self.acceleration.z * self.dt
+
+        # 3. Calculate Position
+        self.position.x += self.velocity.x * self.dt
+        self.position.y += self.velocity.y * self.dt
+        self.position.z += self.velocity.z * self.dt
+
+        # 4 Make sure there is a ground boundary ( z = 0 )
+        if self.position.z <= 0.0:
+            self.position.z = 0.0
+            # If moving below z = 0 put acceleration and velocity to 0
+            if self.velocity.z < 0.0:
+                self.velocity.z = 0
+                self.acceleration.z = max(0.0, self.acceleration.z)
+
+        return {
+            "position": [self.position.x, self.position.y, self.position.z],
+            "velocity": [self.velocity.x, self.velocity.y, self.velocity.z],
+            "acceleration": [self.acceleration.x, self.acceleration.y, self.acceleration.z]
+        }
